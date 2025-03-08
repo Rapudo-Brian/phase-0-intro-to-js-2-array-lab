eb7abf0dcfbcf7a15d083990c4bc4bcd4c91e2a7
Brian Rapudo Ochieng <rapudobrian@gmail.com>
2025-03-08 17:53:29 +0300

    manipulating arrays

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+console.log(cats);
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+console.log(cats);
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+console.log(cats);
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+console.log(cats);
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+console.log(cats);
+
+function appendCat(name) {
+    return [...cats, name];
+}
+console.log(appendCat('Broom'));
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+console.log(prependCat('Broom'));
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+console.log(removeLastCat());
+
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+console.log ( cats.slice(1)); 
+
